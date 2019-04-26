a918cb07b10b144ab248b1a089a252f8cf2d711f
shreyashgupta88 <shreyashgupta88@gmail.com>
2019-04-26 17:21:15 +0530

    Updates

---
+++ precache-manifest.2aafa22f032a8676c7cd6041a13fcd0a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "37cbaf02ebb366e6199a1e1486d945dc",
+    "url": "/News/static/media/i.37cbaf02.png"
+  },
+  {
+    "revision": "ce46561b5b87134fcd8174365da8eff1",
+    "url": "/News/static/media/bg.ce46561b.jpg"
+  },
+  {
+    "revision": "83164b318c41095d23d34cc2a815ada7",
+    "url": "/News/static/media/Syabab Brush Script-demo.83164b31.woff"
+  },
+  {
+    "revision": "3e228e3fe3097291f6ab4e4c38db93bc",
+    "url": "/News/static/media/NeoBulletin Extruded.3e228e3f.woff"
+  },
+  {
+    "revision": "d37a9a776d21fd59755b",
+    "url": "/News/static/js/runtime~main.a19ba134.js"
+  },
+  {
+    "revision": "80cf591a6715ead3a683",
+    "url": "/News/static/js/main.9566db67.chunk.js"
+  },
+  {
+    "revision": "8a54198ca215577bffa1",
+    "url": "/News/static/js/2.53287691.chunk.js"
+  },
+  {
+    "revision": "80cf591a6715ead3a683",
+    "url": "/News/static/css/main.5a53194c.chunk.css"
+  },
+  {
+    "revision": "8a54198ca215577bffa1",
+    "url": "/News/static/css/2.1f815e00.chunk.css"
+  },
+  {
+    "revision": "66b37b5a37d372cdd3cd2a8561b6c3d6",
+    "url": "/News/index.html"
+  }
+];
